3515e9ee6b7c81e49d31533dad8cae9f82e2a08f
jasonlerit <jasonlerit077@gmail.com>
2025-01-03 20:41:02 +0800

    fix: test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,15 @@
 import Page from "@/app/page"
+import { ReactQueryClientProvider } from "@/components/shared/react-query-client-provider"
 import { render, screen } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 
 describe("Page", () => {
   it("should render correctly", () => {
-    render(<Page />)
+    render(
+      <ReactQueryClientProvider>
+        <Page />
+      </ReactQueryClientProvider>
+    )
     expect(screen.getByText("Hey")).toBeDefined()
   })
 })
